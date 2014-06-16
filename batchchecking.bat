ef9e6d15b240be12ddd155eaeb57d6a0470968c6
Bruce Campbell <wavescholar@gmail.com>
2014-06-16 14:07:08 -0400

    061614a

---
+++ batchchecking.bat
@@ -0,0 +1,18 @@
+set DATESTAMP=%DATE:~10,4%_%DATE:~4,2%_%DATE:~7,2%
+set TIMESTAMP=%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
+set DATEANDTIME=%DATESTAMP%_%TIMESTAMP%
+cd klImageCore 
+git add --all .
+git commit -m "%DATEANDTIME%"
+git push
+cd ../
+cd klMatrixCore 
+git add --all .
+git commit -m "%DATEANDTIME%"
+git push
+cd ../
+cd ProjectFiles 
+git add --all .
+git commit -m "%DATEANDTIME%"
+git push
+cd ../
